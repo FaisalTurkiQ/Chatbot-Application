be5b09095099872d8e5f2f330785602143f0f2ce
Faisal Alqahtani <108127805+FaisalTurkiQ@users.noreply.github.com>
2023-07-07 06:56:37 +0300

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = "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"
+
+while (True):
+    msg = input("ME : ")
+
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo", # specifies the model to use for the chat completion
+        messages = [{"role": "system", "content": msg}],
+        #max_tokens = 1,# maximum number of tokens in the response.
+        temperature = 1, # controls the randomness of the model's output 0.8 more random ,0.2 make it more focused
+        n = 2, #determines the number of completions to generate
+    )
+
+    chat_response = completion.choices[0].message.content
+    print(completion)
+    print("GPT : ",chat_response)
